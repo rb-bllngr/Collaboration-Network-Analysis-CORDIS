7db8b272c51171c0fd0af8bb25e3b0dfc5203e7d
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2026-06-19 11:00:06 +0200

    Create environment setup program

---
+++ envir_setup.R
@@ -0,0 +1,50 @@
+# envir_setup.R: Determine global settings and prepare needed packages for project work
+
+# Data file paths
+PATHS <- list(
+  DATA_DIR = "Data/Raw"
+)
+
+# Source helper functions
+source("functions.R")
+
+# invisible(lapply(paths, dir.create, showWarnings = FALSE, recursive = TRUE))
+
+# List of CRAN packages - every package needed for project beside default packages
+packages <- c(
+  # # Install packages
+  # "data.table",
+  # "dplyr",
+  # "ggcorrplot",
+  # "ggplot2",
+  # "knitr",
+  # "lubridate",
+  # "ranger",
+  # "RColorBrewer",
+  # "readr",
+  # "readxl",
+  # "rmarkdown",
+  # "scales",
+  # "stringr"
+  
+  # Install package for downloading data from EU Commission CORDIS' URLs
+  "httr",
+  # Install package for validating function input
+  "checkmate"
+)
+
+# Set CRAN mirror
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# Install CRAN packages if missing
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    message(paste("Installing CRAN package:", pkg))
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  }
+}
+
+# Inform the user to restart R if needed
+message("If you experience any issues with loaded packages,
+        please restart R and re-run this script.")
