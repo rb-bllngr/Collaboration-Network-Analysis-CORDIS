79400684c064aad93bb10f1c3b3adfd6d5bb039d
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2026-07-03 14:46:35 +0200

    Add correlation matrix plot and implement saving to folder

--- Programs/network_degrees.R
+++ Programs/network_degrees.R
@@ -69,6 +69,24 @@ dt_hill_estimator <- rbindlist(lapply(networks, function(net) {
   # Return data.table object with alpha for each k (and keep programme-column)
   data.table(k = seq_len(n - 1), alpha_k = alpha_k)}), idcol = "programme")
 
+# Build joint degree distribution according to Kolaczyk (2010), pp. 86-87. 
+dt_joint_degrees <- rbindlist(lapply(networks, function(net) {
+  # Get degree of all edges for both nodes connected by the edge
+  edges_degrees <- as_edgelist(net, names = FALSE)
+  degrees <- degree(net)
+
+  # Return data.table with degree of one end and degree of other end of every edge
+  data.table(d1 = c(degrees[edges_degrees[, 1]], degrees[edges_degrees[, 2]]),
+             d2 = c(degrees[edges_degrees[, 2]], degrees[edges_degrees[, 1]]))
+}), idcol = "programme")
+
+# Calculate relative frequency of each degree combination
+# Alternatively: Following Kolaczyk (2010)'s description, first to sort the nodes in all
+# edges to receive d_start <= d_end
+# dt_joint_degrees[, c("d_start", "d_end") := .(pmin(d_start, d_end), pmax(d_start, d_end))]
+dt_joint_degrees <- dt_joint_degrees[, .N, by = .(programme, d1, d2)]
+dt_joint_degrees[, freq := N / sum(N), by = programme]
+
 
 # --- Analysis and Visualization ---------------------------------------------------------
 # Compute summary statistics (and more) on degree and strength information
@@ -99,9 +117,9 @@ plot_degree_hist <-
   scale_x_log10() +
   scale_fill_manual(values = colorblindfriendly()) +
   labs(x = "Grad [log10]", y = "Anzahl Organisationen", fill = "EU-Förderprogramm") +
-  set_theme_lmu() +
+  theme_lmu() +
   theme(legend.position = "top")
-### TODO: SAVE THE PLOT TO /PLOTS
+save_plot_lmu(plot_degree_hist, "degree_histogram.png")
 
 # Histogram for strength
 plot_strength_hist <-
@@ -110,9 +128,9 @@ plot_strength_hist <-
   scale_x_log10() +
   scale_fill_manual(values = colorblindfriendly()) +
   labs(x = "Stärke [log10]", y = "Anzahl Organisationen", fill = "EU-Förderprogramm") +
-  set_theme_lmu() +
+  theme_lmu() +
   theme(legend.position = "top")
-### TODO: SAVE THE PLOT TO /PLOTS
+save_plot_lmu(plot_strength_hist, "strength_histogram.png")
 # Note: Looks basically identical to degree histogram. Validate plausibility by checking
 # whether the majority of organisations are one-off, i.e. most edge weights are close to 1
 # (meaning most pairs of organisations only collaborated on one project together) because
@@ -123,42 +141,61 @@ dt_metrics[, mean(strength / degree, na.rm = TRUE), by = programme]
 # Log-log degree distribution
 plot_degree_distrib <-
   ggplot(dt_degree_distrib, aes(x = degree, y = prob, color = programme)) +
-  geom_point(size = 0.8) +
+  geom_point(size = 0.5, alpha = 0.5) +
   scale_x_log10() +
-  scale_y_log10() +
+  scale_y_log10(labels = scales::label_number(drop0trailing = TRUE)) +
   scale_color_manual(values = colorblindfriendly()) +
   labs(x = "Grad [log10]", y = "Relative Häufigkeit [log10]", color = "EU-Förderprogramm") +
-  set_theme_lmu() +
-  theme(legend.position = "top")
-### TODO: SAVE THE PLOT TO /PLOTS
+  theme_lmu() +
+  theme(legend.position = "top") +
+  guides(color = guide_legend(override.aes = list(alpha = 1, size = 2)))
+save_plot_lmu(plot_degree_distrib, "degree_distribution.png")
 
 # Investigate neighbour-degree behaviour among organisations by plotting knn-degree
 plot_degree_knn <-
   ggplot(dt_metrics[!is.na(knn) & degree != 0],
          aes(x = degree, y = knn, color = programme)) +
-  geom_point(shape = 4, size = 0.8, alpha = 0.3) +
+  geom_point(size = 0.5, alpha = 0.1) +
   scale_x_log10() +
   scale_y_log10() +
   scale_color_manual(values = colorblindfriendly()) +
   labs(x = "Grad [log10]",
        y = "Mittlerer Grad der k-nächsten Nachbarn [log10]",
        color = "EU-Förderprogramm") +
-  set_theme_lmu() +
+  theme_lmu() +
   theme(legend.position = "top") +
   guides(color = guide_legend(override.aes = list(alpha = 1, size = 2)))
-### TODO: SAVE THE PLOT TO /PLOTS
+save_plot_lmu(plot_degree_knn, "degree_knn.png")
 
 # Hill plot
 plot_hill <-
   ggplot(dt_hill_estimator, aes(x = k, y = alpha_k, color = programme)) +
-  geom_point(shape = 1, size = 0.8, alpha = 0.75) +
+  geom_point(size = 0.5, alpha = 0.5) +
   scale_color_manual(values = colorblindfriendly()) +
-  labs(x = "k", y = expression(hat(alpha)[k]), color = "Förderprogramm") +
-  set_theme_lmu() +
+  labs(x = "k", y = expression(hat(alpha)[k]), color = "EU-Förderprogramm") +
+  theme_lmu() +
   theme(legend.position = "top") +
   guides(color = guide_legend(override.aes = list(alpha = 1, size = 2)))
-### TODO: SAVE THE PLOT TO /PLOTS
+save_plot_lmu(plot_hill, "powerlaw_fit_hill.png")
 
 # Degree correlation matrix as image representation of logarithmically-transformed joint
 # degree distribution (c. Kolaczyk (2010), Fig. 4.3)
-### TODO:
+# Alternatively: Sorting the degrees within each edge (with pmin and pmax) leads to all
+# edges having the smaller degree first and the bigger degree second. Therefore, as the
+# first edge is shown on the x-axis, the plot would only show the correlation for the
+# upper left triangle, i.e. the undirected graph makes the rest superfluous as symmetric.
+plot_joint_degree_distrib <-
+  ggplot(dt_joint_degrees, aes(x = d1, y = d2, color = log10(freq))) +
+  geom_tile() +
+  scale_x_log10() +
+  scale_y_log10() +
+  scale_color_viridis_c(option = "plasma", direction = -1) +
+  labs(x = "Grad [log10]", y = "Grad [log10]", color = "Relative Häufigkeit [log10]") +
+  facet_wrap(~ programme) +
+  theme_lmu() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        legend.position = "bottom",
+        legend.direction = "horizontal", 
+        legend.title = element_text(vjust = 0.8))
+save_plot_lmu(plot_joint_degree_distrib, "degree_correlation_matrix.png")
