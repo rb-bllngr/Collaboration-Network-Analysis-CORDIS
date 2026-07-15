d3a24e70d554a742b9868a81a54f80f43d3161eb
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2026-07-15 17:00:43 +0200

    Initialize program to analyse project roles

---
+++ Programs/network_roles.R
@@ -0,0 +1,4 @@
+# network_roles.R: Investigate the organizations' roles in projects they participate in.
+#                  Look at project coordinators and their relationship to an organisations
+#                  centrality/degree for H2020 and HORIZON EUROPE collaboration networks.
+
