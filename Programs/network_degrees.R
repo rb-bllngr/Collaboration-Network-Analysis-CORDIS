1c4ffc17f5547a4d45fdac4bc024a51f93ed82f6
Robin Billinger <135109065+rb-bllngr@users.noreply.github.com>
2026-08-03 19:35:09 +0200

    Investigate low-degree log-log anomalies and perform programme tests/comparisons

--- Programs/network_degrees.R
+++ Programs/network_degrees.R
@@ -138,7 +138,7 @@ dt_metrics[, mean(strength / degree, na.rm = TRUE), by = programme]
 # Log-log degree distribution
 plot_degree_distrib <-
   ggplot(dt_degree_distrib, aes(x = degree, y = prob, color = programme)) +
-  geom_point(size = 0.5, alpha = 0.5) +
+  geom_point(size = 1, alpha = 0.5) +
   scale_x_log10() +
   scale_y_log10(labels = scales::label_number(drop0trailing = TRUE)) +
   scale_color_manual(values = colorblindfriendly()) +
@@ -146,13 +146,93 @@ plot_degree_distrib <-
   theme_lmu() +
   theme(legend.position = "top") +
   guides(color = guide_legend(override.aes = list(alpha = 1, size = 2)))
+
+# Investigate low-degree fall-off from the log-log distribution expectation looking at the
+# role of project size and one-off participation:
+cordis <- readRDS(file.path(PATHS$DATA_INT, "cordis_population.RDS"))
+dt_project_size <- cordis[, .(project_size = uniqueN(organisationID)),
+                          by = .(frameworkProgramme, projectID)]
+
+# Attach project size to every organisation-project row and aggregate to organisation-level
+dt_organisations_projects <- merge(cordis[, .(frameworkProgramme, projectID, organisationID)],
+                                   dt_project_size, by = c("frameworkProgramme", "projectID"))
+dt_organisations_projects <- dt_organisations_projects[, .(
+  consortium_mean = mean(project_size),
+  consortium_med = median(project_size)
+), by = .(frameworkProgramme, organisationID)]
+
+# Rebuild degree information with 'organisationID' retained ('dt_metrics' drops it) and
+# merge with consortium information
+dt_degree <- rbindlist(list(
+  data.table(programme = "H2020", organisationID = V(h2020)$name, degree = degree(h2020),
+             n_projects = V(h2020)$n_proj),
+  data.table(programme = "HORIZON", organisationID = V(horizon)$name, degree = degree(horizon),
+             n_projects = V(horizon)$n_proj)
+))
+dt_degree <- merge(dt_degree, dt_organisations_projects,
+                   by.x = c("programme", "organisationID"),
+                   by.y = c("frameworkProgramme", "organisationID"))
+
+# Flag organisations that only participated in exactly one project
+dt_degree[, onetime := (n_projects == 1)]
+
+# Bin the degrees for tabular summary focused on the low-degree region
+dt_degree[, bin := cut(degree, breaks = c(-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 100, Inf),
+                       labels = c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                  "11-25", "26-50", "51-100", "100+"))]
+dt_bin_summary <- dt_degree[degree != 0, .(
+  n_organisations = .N,
+  pct_onetime = mean(onetime) * 100,
+  consortium_mean_mean = mean(consortium_mean)
+), by = .(programme, bin)]
+print(dt_bin_summary[order(programme, bin)])
+
+# Scatter plot of mean consortium size vs. degree
+plot_consortium_degree <-
+  ggplot(dt_degree[degree != 0], aes(x = degree, y = consortium_mean, color = onetime)) +
+  geom_point(size = 1, alpha = 0.15) +
+  scale_x_log10() +
+  scale_y_log10() +
+  scale_color_manual(values = colorblindfriendly(),
+                     labels = c("Teilnahme an mehreren Projekten", "Teilnahme an einem Projekt")) +
+  labs(color = NULL, x = "Grad [log10]",
+       y = "Mittlere Anzahl beteiligter Organisationen pro Projekt [log10]") +
+  facet_wrap(~ programme) +
+  theme_lmu() +
+  theme(legend.position = "top") +
+  guides(color = guide_legend(override.aes = list(alpha = 1, size = 2)))
+
+# Compute for each degree value the share of organisations that are one-time participants
+dt_crossover <- dt_degree[degree != 0, .(
+  pct_onetime = mean(onetime) * 100,
+  n_organisations = .N
+), by = .(programme, degree)]
+setorder(dt_crossover, programme, degree)
+
+# Identify first degree value exceeding (i.e. falling below) the 50% majority threshold
+# Note: Starting at 11 to skip past the naturally high one-time shares among low-degree
+#       organisations (c. 'dt_bin_summary')
+dt_crossover_summary <- dt_crossover[degree > 10 & pct_onetime <= 50][, .SD[1], by = programme]
+print(dt_crossover_summary)
+
+# Overlay crossover point on the degree distribution plot
+plot_degree_distrib <- plot_degree_distrib +
+  geom_vline(data = dt_crossover_summary, aes(xintercept = degree, color = programme),
+             linetype = "twodash")
 save_plot_lmu(plot_degree_distrib, "degree_distribution.png")
+
+# Add the theoretical one-time identity line to the consortium-degree plot, i.e. for an
+# organisation with exactly one project, degree = consortium - 1 by definition, therefore
+# consortium = degree + 1
+plot_consortium_degree <- plot_consortium_degree +
+  geom_function(fun = function(x) x + 1, color = lmu_default_color(), linetype = "twodash")
+save_plot_lmu(plot_consortium_degree, "degree_consortiumsize_lowdegree.png")
 
 # Investigate neighbour-degree behaviour among organisations by plotting knn-degree
 plot_degree_knn <-
   ggplot(dt_metrics[!is.na(knn) & degree != 0],
          aes(x = degree, y = knn, color = programme)) +
-  geom_point(size = 0.5, alpha = 0.1) +
+  geom_point(size = 1, alpha = 0.1) +
   scale_x_log10() +
   scale_y_log10() +
   scale_color_manual(values = colorblindfriendly()) +
@@ -167,7 +247,7 @@ save_plot_lmu(plot_degree_knn, "degree_knn.png")
 # Hill plot
 plot_hill <-
   ggplot(dt_hill_estimator, aes(x = k, y = alpha_k, color = programme)) +
-  geom_point(size = 0.5, alpha = 0.5) +
+  geom_point(size = 1, alpha = 0.5) +
   scale_color_manual(values = colorblindfriendly()) +
   labs(x = "k", y = expression(hat(alpha)[k]), color = "EU-Förderprogramm") +
   theme_lmu() +
@@ -196,3 +276,32 @@ plot_joint_degree_distrib <-
         legend.direction = "horizontal", 
         legend.title = element_text(vjust = 0.8))
 save_plot_lmu(plot_joint_degree_distrib, "degree_correlation_matrix.png")
+
+# Perform some cross-programme distributional comparisons using statistical tests and more:
+# Two-sample Kolmogorov-Smirnov test on degree distributions testing whether H2020
+# and HORIZON degree sequences follow the same underlying distribution. Exploratory only,
+# as degrees within network are not independent draws! Ties in the integer degree values
+# prevent exact p-value, hence exact = FALSE.
+test_kolmogorov_smirnov_degree <- ks.test(dt_metrics[programme == "H2020", degree],
+                                          dt_metrics[programme == "HORIZON", degree],
+                                          alternative = "two.sided", exact = FALSE)
+print(test_kolmogorov_smirnov_degree)
+
+# Wilcoxon rank-sum test on degrees to test whether one programmes' degrees are
+# systematically higher/lower than the other's, in addition to distributional KS-Test
+test_wilcoxon_degree <- wilcox.test(degree ~ programme, data = dt_metrics,
+                                    alternative = "two.sided", conf.int = TRUE)
+print(test_wilcoxon_degree)
+
+# Degree-strength scaling exponent by fitting strength and degree, respectively, in
+# logarithmic LM per programme to test near-1 strength/degree ratio observed above
+dt_lm_fit_degree_strength <- dt_metrics[(degree != 0) & (strength != 0)]
+fit_degree_strength <- lm(log(strength) ~ log(degree) * programme,
+                          data = dt_lm_fit_degree_strength)
+print(summary(fit_degree_strength))
+
+# Degree-knn scaling exponent by fitting knn and degree, respectively, in logarithmic LM
+# per programme to test degree-correlation pattern shown in 'plot_degree_knn'
+dt_lm_fit_degree_knn <- dt_metrics[(is.na(knn) == FALSE) & (degree != 0) & (knn != 0)]
+fit_degree_knn <- lm(log(knn) ~ log(degree) * programme, data = dt_lm_fit_degree_knn)
+print(summary(fit_degree_knn))
